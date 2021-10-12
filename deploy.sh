6dfa7a305d9535138304000a66ee0f394cdfc96a
Izumi <1042631369@qq.com>
2021-10-12 10:10:06 +0800

    增加部署文件选项

--- deploy.sh
+++ deploy.sh
@@ -12,31 +12,27 @@ echo "$imageName"
 
 containerId=`docker ps -a | grep -w ${project_name}:${tag} | awk '{print $1}'`
 
+if [ "${containerId}" != "" ] ; then
+	docker stop $containerId
+
+	docker rm $containerId
+
+	docker container prune -f
+fi
+
+imageId=`docker images | grep -w $project_name | awk '{print $3}'`
+
+if [ "${imageId}" != "" ] ; then
+
+        docker rmi -f $imageId
+
+	docker image prune -f
+
+fi
 
 docker login -u Izumi -p Qwer3936134 $harbor_url
 
 docker pull $imageName
-
-oldImageId=`docker images | grep -w ${project_name} | awk 'NR==2' | awk '{print $3}'`
-
-oldContainerId=`docker ps -a | grep -w ${oldImageId} | awk '{print $1}'`
-
-
-if [ "${containerId}" != "" ] ; then
-        docker stop $containerId
-
-        docker rm $containerId
-
-        docker container prune -f
-fi
-
-if [ "${oldImageId}" != "" ] ; then
-
-        docker rmi -f $oldImageId
-
- docker image prune -f
-
-fi
 
 docker run -d -p $port:$port $imageName
 
