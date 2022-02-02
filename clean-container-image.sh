d620b5c99c0c847e83b709c84a9626a8d22c3d07
TW_Renyong Wang <renyong.wang@thoughtworks.com>
2022-02-02 18:42:14 +0800

    jenkins-rachael-back-end

--- clean-container-image.sh
+++ clean-container-image.sh
@@ -9,17 +9,9 @@ imageName=$harbor_url/$harbor_project_name/$project_name:$tag
 
 echo "$imageName"
 
-containerId=`docker ps -a | grep -w ${project_name}:${tag} | awk '{print $1}'`
-
-if [ "${containerId}" != "" ] ; then
-	docker stop $containerId
-
-	docker rm $containerId
-
-	docker container prune -f
-fi
-
 imageId=`docker images | grep -w $project_name | awk '{print $3}'`
+
+echo "$imageId"
 
 if [ "${imageId}" != "" ] ; then
 
